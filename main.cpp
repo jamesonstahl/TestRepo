27320342d3b49371f2a451fa5d0111c852ccb955
Jameson Stahl <jstahl@carthage.edu>
2021-02-03 14:17:21 -0600

    Added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+ void printName(){
+     cout << "Jameson Stahl" << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    printName();
+
+    return 0;
+}
